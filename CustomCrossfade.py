5bcab0bf789e16fe60bce6a3e99ffd28d9af5db7
Jacob Leis <jacobleis@gmail.com>
2021-02-14 15:30:04 -0700

    stable version of the concept

---
+++ CustomCrossfade.py
@@ -0,0 +1,200 @@
+""" Custom Crossfade needs documentation.
+
+"""
+
+# PSL Imports
+import base64
+import json
+from time import sleep
+
+# 3rd Party Imports
+import requests
+import toml
+
+# Project Imports
+secrets = toml.load('secrets.toml')
+
+# Constants
+clientId = secrets['ClientInfo']['clientId']
+clientSecret = secrets['ClientInfo']['clientSecret']
+
+# Loading Old Token
+with open('oldtoken.txt', 'r') as reader:
+    refreshtoken = reader.read()
+reader.close()
+
+# https://accounts.spotify.com/authorize?client_id=e35e8d9e13754f3389657a3ded64b4ea&response_type=code&redirect_uri=https%3A%2F%2Fwintermindgroup.com%2F&scope=user-read-private%20user-read-email%20user-modify-playback-state
+code = 'AQA8D2oGd31yechZ06IjvEDD3pRVw5c5TiH4XFKFj2lxDecFLdOW8iD02XK4Sdoykr_C20Eyvhry20tCWtVCART9E0H57WR-OVeV6obho5TfvFvRGTr0G5EKqP45bs0ydu9mJRUozcR7TVUfuAGQ_i5Tbd-XTj0Bg90kQf9VF-5ek2zwjzhwZ_4zSU5w9GjmApMp6AokfO3p5JncGPq3KWkdRTk'
+token = 'BQBv8gAgd5C5J1-dH3tfBPofWQFIUHNwFbDNTdAibQWhgwCAbcc4VrxabjifUPXcH3CAarZ5fOwkjBITuDdlV2Cvw_fMNmL4guFRpj4FcyVS7RtTVVvCbZSHJKY9nObhB_MCY1rvjyIUJyCbysZlbPOvl9qOfdD53Q'
+
+# Song List [URI, Skip, Wait]
+
+SongList = [
+    ['spotify:track:0YPDp1KIxVLTdh3vnvk6wd', '11000', '20'], 
+    ['spotify:track:1JcGNoiwifg0MdJMVgJQYx', '0', '30'],
+    ['spotify:track:6tedQ1ZmbygqhbdcfJL7Xb', '9000', '21']
+        ]
+
+
+# Functions
+#################################################################################
+
+# Converting Strings to Base64 Encoding
+def strToBase64(StrToConvert):
+    StrInBytes = StrToConvert.encode('ascii')
+    Base64Bytes = base64.b64encode(StrInBytes)
+    StrConverted = Base64Bytes.decode('ascii')
+
+    return StrConverted
+
+
+# Get token from code
+def GetTokensFromCode(authStr, code, RedirectURI):
+
+    url = 'https://accounts.spotify.com/api/token'
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization' : f'Basic {authStr}'
+        }
+
+    data = {
+    'grant_type': 'authorization_code',
+    'code': f'{code}',
+    'redirect_uri': f'{RedirectURI}'
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    
+    return response
+
+# Refresh Token
+def GetNewAccessToken(RefreshToken):
+
+    url = 'https://accounts.spotify.com/api/token'
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization' : f'Basic {authStr}'
+        }
+
+    data = {
+    'grant_type': 'refresh_token',
+    'refresh_token': f'{RefreshToken}'
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+
+    return response.json()['access_token']
+
+
+# Adding song to queue. user-modify-playback-state
+def AddSongToQueue(SpotURI):
+
+    headers = {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization': f'Bearer {token}'
+            }
+    
+    params = (
+        ('uri', f'{SpotURI}'),
+            )
+
+    response = requests.post('https://api.spotify.com/v1/me/player/queue', headers=headers, params=params)
+    
+    try:
+        print(json.dumps(response.json(), indent=2))
+    except:
+        None
+
+    return True
+
+
+# Adding song to queue. user-modify-playback-state
+def SkipToNextSong():
+
+    headers = {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization': f'Bearer {token}'
+            }
+
+    response = requests.post('https://api.spotify.com/v1/me/player/next', headers=headers)
+
+    try:
+        print(json.dumps(response.json(), indent=2))
+    except:
+        None
+
+    return True
+
+# Skip to spot in track. user-modify-playback-state
+def SeekToTime(TimeMS):
+
+    url = f'https://api.spotify.com/v1/me/player/seek?position_ms={TimeMS}'
+
+    headers = {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization': f'Bearer {token}'
+            }             
+    
+    print(url)
+
+    response = requests.put(url=url, headers=headers)
+    # print(response.status_code)
+    # print(response.content)
+
+    try:
+        print(json.dumps(response.json(), indent=2))
+    except:
+        None
+
+    return True
+
+
+
+
+authStr = strToBase64(f'{clientId}:{clientSecret}')
+RedirectURI = 'https://wintermindgroup.com/'
+
+
+print('Did you need a new token? (y/n)')
+UserInput = input()
+
+if UserInput == 'y':
+    print('Gimme that auth code:')
+    code = input()
+    print('Grabbing new token.')
+    response = GetTokensFromCode(authStr, code, RedirectURI)
+    token = response.json()['access_token']
+    refreshtoken = response.json()['refresh_token']
+    with open('oldtoken.txt', 'w') as writer:
+        writer.write(refreshtoken)
+    writer.close()
+
+# Adding Songs to Queue
+for Song in SongList:
+    print('Adding a song to up next.')
+    token = GetNewAccessToken(refreshtoken)
+    AddSongToQueue(Song[0])
+    print('Song Added')
+
+print('Skipping to next song.')
+token = GetNewAccessToken(refreshtoken)
+SkipToNextSong()
+
+for Song in SongList:
+    if int(Song[1]) > 0:
+        print(f'Skipping to {Song[1]}')
+        token = GetNewAccessToken(refreshtoken)
+        SeekToTime(Song[1])    
+    print(f'Sleeping for {Song[2]}')
+    sleep(int(Song[2]))
+    token = GetNewAccessToken(refreshtoken)
+    SkipToNextSong()
+
+
+
+print('All Done.')
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 Custom Crossfade for Spotiy
 
 This python script allows you to custom skip to specific in and outs in a spotify playlist.
-
-
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
